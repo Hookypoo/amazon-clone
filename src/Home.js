0b85b81d638860fb323fab009aeb5ee68ea28227
Hookypoo <forde.hugh@yahoo.com>
2020-11-16 23:06:47 -0400

    added header links and styles

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+    return (
+        <div className="home">
+            <img className="home__image"
+                src="https://images-eu.ssl-images-amazon.com/images/G/02/digital" alt="" />
+        </div>
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB5F6mqoyxf5oKwImy0euZlVU8tUcY4okQ",
+    authDomain: "clone-2a19b.firebaseapp.com",
+    databaseURL: "https://clone-2a19b.firebaseio.com",
+    projectId: "clone-2a19b",
+    storageBucket: "clone-2a19b.appspot.com",
+    messagingSenderId: "559134512816",
+    appId: "1:559134512816:web:78aaaa0606f10a4f910d75",
+    measurementId: "G-MFS2FKK14Q"
+};
