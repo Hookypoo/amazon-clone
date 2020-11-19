e20b4a1c57ebec1038e43226a8ccd5cc71ca3b17
Hookypoo <forde.hugh@yahoo.com>
2020-11-18 22:49:42 -0400

    created product card...not finished

--- src/Product.js
+++ src/Product.js
@@ -4,20 +4,23 @@ import "./Product.css"
 function Product({ id, title, image, price, rating }) {
     return (
         <div className="product">
-            <p>{title}</p>
-            <p className="product__price">
-                <small>$</small>
-                <strong>{price}</strong>
-            </p>
-            <div className="product__rating">
-                {
-                    Array(rating)
-                        .fill()
-                        .map((_) => (
-                            <p></p>
-                        ))
-                }
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {
+                        Array(rating)
+                            .fill()
+                            .map((_) => (
+                                <p ><img className="product__starRating" src="https://img.icons8.com/emoji/2x/star-emoji.png" /></p>
+                            ))}
+                </div>
             </div>
+            <img className="product__image" src={image} alt="" />
+            <button>Add to Basket</button>
         </div>
     )
 }
