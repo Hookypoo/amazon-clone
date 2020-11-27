cf54a92e02b95c731c664a970272f215db260f44
Hookypoo <forde.hugh@yahoo.com>
2020-11-26 21:43:16 -0400

    configured auth,started login page

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
+import { auth } from "./firebase";
+import { Link, useHistory } from "react-router-dom";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = event => {
+        event.preventDefault(); // this stops auto page refresh on submit/click
+
+        //do login logic here....
+        auth.signInWithEmailAndPassword(email, password)
+            .then((auth) => {
+                //logged in, so redirect to homepage
+
+            })
+            .catch(e => alert(e.message))
+    };
+
+    const register = event => {
+        event.preventDefault(); // this stops auto page refresh on submit/click
+
+        //do registration logic here...
+    }
+
+
+
     return (
         <div className="login">
-            <h1>Login Page Hooky</h1>
+            <Link to="/">
+                <img
+                    className="login__logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+                    alt=""
+                />
+            </Link>
+            <div className="login__container">
+                <h1>Sign in</h1>
+                <form>
+                    <h5>Email</h5>
+                    <input value={email} onChange={event => setEmail(event.target.value)} type="email" />
+                    <h5>Password</h5>
+                    <input value={password} onChange={event => setPassword(event.target.value)} type="password" />
+                    <button onClick={login} type="submit" className="login__signInButton">Sign in</button>
+                </form>
+
+                <p>
+                    By signing-in you agree to Amazon's conditions of Use and Sales.
+                    Please see our Privacy Notice, our Cookies Notice and our Interest Based Ads Notice.
+                </p>
+                <button onClick={register} className="login__registerButton">Create Your Amazon Account</button>
+            </div>
+
         </div>
     )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
+import firebase from "firebase";
+
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyB5F6mqoyxf5oKwImy0euZlVU8tUcY4okQ",
     authDomain: "clone-2a19b.firebaseapp.com",
     databaseURL: "https://clone-2a19b.firebaseio.com",
@@ -8,4 +11,9 @@ const firebaseConfig = {
     messagingSenderId: "559134512816",
     appId: "1:559134512816:web:78aaaa0606f10a4f910d75",
     measurementId: "G-MFS2FKK14Q"
-};
+});
+
+// const db = firebasdeApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
